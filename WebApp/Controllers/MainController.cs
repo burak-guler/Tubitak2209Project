d4772dc00262ec994e7b4db9eb739a104980c799
Umut Ülke <Umutberke26x@outlook.com>
2023-11-03 00:59:52 +0300

    template compated

---
+++ WebApp/Controllers/MainController.cs
@@ -0,0 +1,14 @@
+﻿using Gat.DataAccessLayer.Concrete.GatContext;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace WebApp.Controllers
+{
+    public class MainController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
